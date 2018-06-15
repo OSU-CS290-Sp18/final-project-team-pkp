8d970f3433c27af4573735981c6d1ecb33d2a5bf
sheltoti <38222463+sheltoti@users.noreply.github.com>
2018-06-14 18:46:37 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,71 +1,37 @@
-/*"Most recent variable" for storing which type of shop has been clicked most recently, to be passed into show information*/
-var MostRecentShop = '';
+ /*Enables the capacity to access the "hidden" element of the modals and edit it*/
+var RemoveHiddenAccess = document.getElementsByClassName('hidden');                            
 
-/*Listeners on each shop type of button so I can pass to mostrecent value*/
-var FastFoodButton = document.getElementsByClassName('shop-fastFood-button');
-var GroceryStoreButton = document.getElementsByClassName('shop-groceryStore-button');
-var PharmacyButton = document.getElementsByClassName('shop-pharmacy-button');
-var BarsButton = document.getElementsByClassName('shop-bars-button');
-var CoffeeButton = document.getElementsByClassName('shop-coffee-button');
-
-/*Listeners on these buttons that call the updateMostRecent shop selection function*/
-FastFoodButton[0].addEventListener('click',updateMostRecent('shop-fastFood-button'));
-GroceryStoreButton[0].addEventListener('click',updateMostRecent('shop-groceryStore-button'));
-PharmacyButton[0].addEventListener('click',updateMostRecent('shop-pharmacy-button'));
-BarsButton[0].addEventListener('click',updateMostRecent('shop-bars-button'));
-CoffeeButton[0].addEventListener('click',updateMostRecent('shop-coffee-button'));
-/*Function that updates the MostRecentShop variable so we can effectively fill out the showinformation correctly*/
-function updateMostRecent(whatWasClicked){
-  console.log(whatWasClicked);
-  MostRecentShop = whatWasClicked;
-}
-                               
 /*Connect The HTML elements of the Close/Cancel/Find buttons to be available to the event listeners*/
 var ModalCloseButton = document.getElementsByClassName('modal-close-button');
 var ModalCancelButton = document.getElementsByClassName('modal-cancel-button');
-var ModalFindButton = document.getElementsByClassName('modal-find-button');
+
+/*Information modal button/listener, to be applied to all 5 buttons*/
+var CreateListenerUnhideInformationModal = document.getElementsByClassName('shop-button');
+CreateListenerUnhideInformationModal.addEventListener('click',showInformationModal);
 
 /*Connect the HTML element of the "Add shop" button so it can be listened for*/
 var AddShopButton = document.getElementsByClassName('shop-addshop-button');
 AddShopButton[0].addEventListener('click',showCreateShopModal);
 
-/*Listens for a click on the choose category button*/
-var CreateListenerUnhideInformationModal = document.getElementById('shop-choose-button');
-CreateListenerUnhideInformationModal.addEventListener('click',showInformationModal);
-/*Enables the capacity to access the "hidden" element of the modals and edit it*/
-var RemoveHiddenAccess = document.getElementsByClassName('hidden');
 /*Create the capacity to, and listen for a click on the show button for search*/
 var UnhideSearchModal = document.getElementById('shop-search-button');
 UnhideSearchModal.addEventListener('click',showSearchModal);
-
-/*Shows the modal when the choose button is clicked*/
+/****************************************************************************/
+/*Shows the information modal when the choose button is clicked*/
 function showInformationModal() {
   /*New style of getting to change the 'hidden' attribute*/
   RemoveHiddenAccess[0].style['display'] = 'block';
   RemoveHiddenAccess[1].style['display'] = 'block';
-  
   /*Call hide when close or cancel are clicked*/
   ModalCloseButton[0].addEventListener('click',hideInformationModal);
   ModalCancelButton[0].addEventListener('click',hideInformationModal);
-/*Create the functionality this calls*/ /*Debugging/Dont need this rightnow*/ /*ModalFindButton[0].addEventListener('click',FindLocation);  */
-  
-  /*Temporarily Icing your code to see if my way works*/
-  /*var modalBackdrop = document.getElementById('modal-backdrop');
-  var createShopModal = document.getElementById('create-shop-modal');
-  modalBackdrop.classList.remove('hidden');
-  createShopModal.classList.remove('hidden');*/
 }
-/*Hides the modal when close or cancel buttons are clicked*/
+/*Hides the information modal when close or cancel buttons are clicked*/
 function hideInformationModal() {
   RemoveHiddenAccess[0].style['display'] = 'none';
   RemoveHiddenAccess[1].style['display'] = 'none';
-  /*Icing your code again mybad*/
-  /*var modalBackdrop = document.getElementById('modal-backdrop');
-  var createShopModal = document.getElementById('create-shop-modal');
-  modalBackdrop.classList.add('hidden');
-  createShopModal.classList.add('hidden');*/
 }
-
+/*************************************************************************/
 /*Unhides the create shop modal and background when called*/
 function showCreateShopModal() {
   RemoveHiddenAccess[2].style['display'] = 'block';
@@ -79,7 +45,7 @@ function hideCreateShopModal() {
   RemoveHiddenAccess[2].style['display'] = 'none';
   RemoveHiddenAccess[3].style['display'] = 'none';
 }
-
+/***************************************************************************/
 /*Shows the search content modal*/
 function showSearchModal(){
   RemoveHiddenAccess[4].style['display'] = 'block';
@@ -88,13 +54,12 @@ function showSearchModal(){
   ModalCloseButton[2].addEventListener('click',hideSearchModal);
   ModalCancelButton[2].addEventListener('click',hideSearchModal);
 }
-
 /*Hides the search content modal*/
 function hideSearchModal() {
   RemoveHiddenAccess[4].style['display'] = 'none';
   RemoveHiddenAccess[5].style['display'] = 'none';
 }
-
+*****************************************************************************/
 /*Wait for DOM content to load, then hook up UI interactions*/
 window.addEventListener('DOMContentLoaded', function () {
 
@@ -128,3 +93,39 @@ window.addEventListener('DOMContentLoaded', function () {
     searchInput.addEventListener('input', doSearchUpdate);
   }*/
 });
+/*Code GraveYard*/
+/*
+/*Listens for a click on the choose category button*/
+
+  /*Icing your code again mybad*/
+  /*var modalBackdrop = document.getElementById('modal-backdrop');
+  var createShopModal = document.getElementById('create-shop-modal');
+  modalBackdrop.classList.add('hidden');
+  createShopModal.classList.add('hidden');*/
+/*"Most recent variable" for storing which type of shop has been clicked most recently, to be passed into show information*/
+/*var MostRecentShop = '';*/
+/*Listeners on each shop type of button so I can pass to mostrecent value*/
+/*var FastFoodButton = document.getElementsByClassName('shop-fastFood-button');
+var GroceryStoreButton = document.getElementsByClassName('shop-groceryStore-button');
+var PharmacyButton = document.getElementsByClassName('shop-pharmacy-button');
+var BarsButton = document.getElementsByClassName('shop-bars-button');
+var CoffeeButton = document.getElementsByClassName('shop-coffee-button');
+*/
+/*Listeners on these buttons that call the updateMostRecent shop selection function*/
+/*FastFoodButton[0].addEventListener('click',updateMostRecent('shop-fastFood-button'));
+GroceryStoreButton[0].addEventListener('click',updateMostRecent('shop-groceryStore-button'));
+PharmacyButton[0].addEventListener('click',updateMostRecent('shop-pharmacy-button'));
+BarsButton[0].addEventListener('click',updateMostRecent('shop-bars-button'));
+CoffeeButton[0].addEventListener('click',updateMostRecent('shop-coffee-button'));*/
+/*Function that updates the MostRecentShop variable so we can effectively fill out the showinformation correctly*/
+/*function updateMostRecent(whatWasClicked){
+  console.log(whatWasClicked);
+  MostRecentShop = whatWasClicked;
+}*/
+/*Create the functionality this calls*/ /*Debugging//*ModalFindButton[0].addEventListener('click',FindLocation);  */
+  
+  /*Temporarily Icing your code to see if my way works*/
+  /*var modalBackdrop = document.getElementById('modal-backdrop');
+  var createShopModal = document.getElementById('create-shop-modal');
+  modalBackdrop.classList.remove('hidden');
+  createShopModal.classList.remove('hidden');*/
