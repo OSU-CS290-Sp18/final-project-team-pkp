ed09542ef62073615c04ac6a728d45cc77d77e56
hansohay <32318844+hansohay@users.noreply.github.com>
2018-06-14 21:08:18 -0700

    Create server.js
    created the server.js file

---
+++ server.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoURL = "mongodb://" + mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDBName;
+console.log(mongoURL);
+
+var mongoDB = null;
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res, next) {
+  res.status(200).render('home');
+});
+
+
+
+
+app.use('*', function (req, res, next) {
+  res.status(404).render('404');
+});
+
+mongoClient.connect(mongoURL, function (err, client){
+  if(err){
+    throw err;
+  }
+  mongoDB = client.db(mongoDBName);
+  app.listen(port, function () {
+    console.log("== Server listening on port", port);
+  });
+})
+
